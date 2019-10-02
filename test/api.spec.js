1744a2a4cd4d1d97faa33d69224a1f5f2b9f3c7c
sam_at_NT <sangram.lad@nationaltrust.org.uk>
2019-10-02 14:02:04 +0100

    completed UI tests

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,19 +2,33 @@
  * Created by brett.hadley on 10/10/2016.
  */
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require("chai-as-promised");
 const getData = require('../src/api').getData;
+const getVehicleData = require('../src/api').getVehicleData;
 const server = require('../server');
 
+chai.use(chaiAsPromised);
+
 describe("getData example test", function() {
-    beforeEach(() => {
+    before(() => {
         server.listen(9988);
     });
-    it('should respond with an array of vehicles', (done) => {
-        getData().then((response) => {
-            const data = JSON.parse(response);
-            expect(Array.isArray(data.vehicles)).to.equal(true);
-            done();
-        })
+    it('should respond with an array of vehicles', () => {
+        const vehiclesPromise = getData();
+
+        return vehiclesPromise.then((response) => {
+            expect(response.vehicles.length).to.equal(5);
+            expect(response.vehicles[0].id).to.equal("xe");
+        });
+    });
+    it('should respond with an array of vehicles', () => {
+        const vehiclesPromise = getVehicleData("xe");
+
+        return vehiclesPromise.then((data) => {
+            expect(data.id).to.equal("xe");
+            expect(data.price).to.equal("£30,000");
+        });
     });
 });
